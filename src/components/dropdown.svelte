2044f7dce5be3c0ba1dd34b4a851a32636f4fa8d
Pontus <pontus.z94@gmail.com>
2023-11-13 15:14:35 +0100

    added correct label var instead of string

--- src/components/dropdown.svelte
+++ src/components/dropdown.svelte
@@ -25,7 +25,7 @@
         label = "Required"
       } else {
         notValid = false
-        label = "Feedback category"
+        label = label
       }
       isOpen = false
     }
@@ -37,7 +37,7 @@
       label = "Required"
     } else {
       notValid = false
-      label = "Feedback category"
+      label = label
     }
     isOpen = !isOpen
   }
